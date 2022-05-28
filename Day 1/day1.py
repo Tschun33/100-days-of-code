5dcbabb801eed1168788f723b4747a77c90c4993
Stefan Baltschun <StefanBaltschun@web.de>
2022-05-28 07:27:56 +0200

    init

---
+++ Day 1/day1.py
@@ -0,0 +1,11 @@
+print('Hello User!')
+city = input('What city did you grew up in?\n')
+pet = input('What was the name of your pet?\n')
+
+band_name = city + " " + pet
+
+print('Your bandname could be ' + band_name)
+
+
+
+
