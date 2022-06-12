7aff69dc43768f5a8a4f3cce0d1e4dc4c5b83571
Stefan Baltschun <StefanBaltschun@web.de>
2022-06-12 17:27:31 +0200

    day 10 finished logic for calculator

--- day 10/day10.py
+++ day 10/day10.py
@@ -1,19 +1,17 @@
 calculating = True
+still_calculating = "y"
 
 
 def calc(number1, operator_calc, number2):
     if operator_calc == "+":
-        result = number1 + number2
-        print(f"{number1} {operator_calc} {number2} = {result}")
+        return number1 + number2
     elif operator_calc == "-":
-        result = number1 - number2
-        print(f"{number1} {operator_calc} {number2} = {result}")
+        return number1 - number2
     elif operator_calc == "/":
-        result = number1 / number2
-        print(f"{number1} {operator_calc} {number2} = {result}")
+        return number1 / number2
     elif operator_calc == "*":
-        result = number1 * number2
-        print(f"{number1} {operator_calc} {number2} = {result}")
+        return number1 * number2
+
     else:
         print("No valid Operator")
 
@@ -22,7 +20,25 @@ while calculating:
     num1 = int(input("What is your first number?"))
     operator = input("Chose an operator:")
     num2 = int(input("What is your second number"))
-    calc(num1, operator, num2)
+    result_calc = calc(num1, operator, num2)
+    print(f"{num1} {operator} {num2} = {result_calc}")
+    still_calculating = input("Continue calculating? Type 'y' or 'n'")
+    while still_calculating == "y":
+        operator_next = input("Next operator: ")
+        num_next = int(input("Next number: "))
+        result_next = calc(result_calc, operator_next, num_next)
+        print(f"{result_calc} {operator_next} {num_next} = {result_next}")
+        result_calc = result_next
+        still_calculating = input("Continue calculating? Type 'y' or 'n'")
+    if input("Next calculation? Type 'y' or 'n'") == "n":
+        calculating = False
+    else:
+        calculating = True
+
+
+
+
+
 
 
 
