894148d2f2f34c316538eaad6decbab8776ecea7
Stefan Baltschun <StefanBaltschun@web.de>
2022-06-20 15:45:29 +0200

    start day 15 coffee machine

--- day 15/day15.py
+++ day 15/day15.py
@@ -3,9 +3,21 @@ import math
 import Menu
 
 
-def check_coins(penny, nickel, dime, quarter):
+def check_coins(penny, nickel, dime, quarter, price):
     sum_coins = round(penny * 0.01 + nickel * 0.05 + dime * 0.10 + quarter * 0.25, 2)
     return sum_coins
+
+
+def print_resources():
+    water = Menu.resources["water"]
+    milk = Menu.resources["milk"]
+    coffee = Menu.resources["coffee"]
+    print(f"water: {water}, milk: {milk}, coffee: {coffee}")
+
+
+def return_change(sum, price):
+    change = sum - price
+    print(f"Your change is: ${change}")
 
 
 def check_resources(beverage):
@@ -25,11 +37,17 @@ def check_resources(beverage):
 machine_is_running = True
 
 while machine_is_running:
-    penny_count = int(input("Pennies:"))
-    nickel_count = int(input("Nickels:"))
-    dime_count = int(input("Dimes:"))
-    quarter_count = int(input("Quarters:"))
-    coin_sum = check_coins(penny_count, nickel_count, dime_count, quarter_count)
-    print(f"The total value is ${coin_sum}")
-    machine_is_running = False
+    choice = input("Espresso Capucchino or Latte ?")
+    choice = choice.lower()
+    choice_customer = Menu.MENU[choice]
+    if check_resources(choice_customer):
+        penny_count = int(input("Pennies:"))
+        nickel_count = int(input("Nickels:"))
+        dime_count = int(input("Dimes:"))
+        quarter_count = int(input("Quarters:"))
+        coin_sum = check_coins(penny_count, nickel_count, dime_count, quarter_count, choice_customer["cost"])
+        print(f"The total value is ${coin_sum}")
+    if choice == "stop":
+        machine_is_running = False
 
+print(Menu.MENU["latte"])
