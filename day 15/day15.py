a71a96c9f67645173641083ca1399392b677f7bc
Stefan Baltschun <StefanBaltschun@web.de>
2022-06-20 19:09:35 +0200

    start day 15 coffee machine

--- day 15/day15.py
+++ day 15/day15.py
@@ -1,11 +1,21 @@
 import math
 
 import Menu
+balance = 0
+machine_is_running = True
 
 
-def check_coins(penny, nickel, dime, quarter, price):
+def check_coins(penny, nickel, dime, quarter):
     sum_coins = round(penny * 0.01 + nickel * 0.05 + dime * 0.10 + quarter * 0.25, 2)
     return sum_coins
+
+
+def check_price(sum_coins, price):
+    if price > sum_coins:
+        print("Not enough money!")
+        return False
+    else:
+        return True
 
 
 def print_resources():
@@ -15,8 +25,8 @@ def print_resources():
     print(f"water: {water}, milk: {milk}, coffee: {coffee}")
 
 
-def return_change(sum, price):
-    change = sum - price
+def return_change(sum_coins, price):
+    change = sum_coins - price
     print(f"Your change is: ${change}")
 
 
@@ -34,10 +44,14 @@ def check_resources(beverage):
         return True
 
 
-machine_is_running = True
+def make_beverage(beverage):
+    Menu.resources["water"] -= beverage["water"]
+    Menu.resources["milk"] -= beverage["milk"]
+    Menu.resources["coffee"] -= beverage["coffee"]
+
 
 while machine_is_running:
-    choice = input("Espresso Capucchino or Latte ?")
+    choice = input("Espresso Cappuccino or Latte ?")
     choice = choice.lower()
     choice_customer = Menu.MENU[choice]
     if check_resources(choice_customer):
@@ -45,8 +59,14 @@ while machine_is_running:
         nickel_count = int(input("Nickels:"))
         dime_count = int(input("Dimes:"))
         quarter_count = int(input("Quarters:"))
-        coin_sum = check_coins(penny_count, nickel_count, dime_count, quarter_count, choice_customer["cost"])
+        coin_sum = check_coins(penny_count, nickel_count, dime_count, quarter_count)
         print(f"The total value is ${coin_sum}")
+        if check_price(coin_sum, choice_customer["cost"]):
+            balance += coin_sum
+            make_beverage(choice_customer)
+            return_change(coin_sum, choice_customer["cost"])
+    if choice == "report":
+        print_resources()
     if choice == "stop":
         machine_is_running = False
 
