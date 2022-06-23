6601502c079d41c28f99b94892a83116dfa1f583
Stefan Baltschun <StefanBaltschun@web.de>
2022-06-23 19:41:28 +0200

    fixed the input validation and removed the check of the users choices

--- day 16/main.py
+++ day 16/main.py
@@ -9,14 +9,18 @@ money_machine = MoneyMachine()
 
 while machine_is_on:
     print("Hello!")
-    user_choice = input("What would you like to drink? type: cappuccino, espresso or latte:")
-    if user_choice == "cappuccino" or user_choice == "espresso" or user_choice == "latte":
+    options = menu.get_items()
+    user_choice = input(f"What would you like to drink? type: {options} :")
+    if user_choice == "report":
+        coffee_maker.report()
+    elif user_choice == "stop":
+        machine_is_on = False
+    else:
         drink = menu.find_drink(user_choice)
         if coffee_maker.is_resource_sufficient(drink):
             if money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
-    elif user_choice == "report":
-        coffee_maker.report()
+
 
 
 
