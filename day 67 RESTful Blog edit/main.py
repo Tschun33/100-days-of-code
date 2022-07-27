23921f424ce0d2888c4e21905b6a0fcc4cb34f7b
Stefan Baltschun <StefanBaltschun@web.de>
2022-07-27 08:20:59 +0200

    Startet: - adding new posts to the blog

---
+++ day 67 RESTful Blog edit/main.py
@@ -0,0 +1,102 @@
+import datetime
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = StringField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+@app.route('/')
+def get_all_posts():
+    posts = db.session.query(BlogPost).all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = db.session.query(BlogPost).filter_by(id=index).first()
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+
+    # Notice body's StringField changed to CKEditorField
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+@app.route("/new-post", methods=["GET", "POST"])
+def make_post():
+    time = datetime.datetime.now()
+    form = CreatePostForm()
+    if request.method == "GET":
+        return render_template("make-post.html", form=form)
+    if request.method == "POST":
+        new_post = BlogPost(
+            title=form.title.data,
+            author=form.author.data,
+            subtitle=form.subtitle.data,
+            date=f"{time.strftime('%B')} {time.day}, {time.year}",
+            body=form.body.data,
+            img_url=form.img_url.data,
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
--- test/time.py
+++ test/time.py
@@ -3,15 +3,18 @@ import random
 
 import requests as requests
 
-time = datetime.datetime.now().strftime("%Y")
+
 parameters = {
     "name": "Stefan"
 }
 r = requests.get("https://api.genderize.io", params=parameters)
 r.raise_for_status()
 r = r.json()["gender"]
-print(r)
+
 
 
 t_list = ["Hello", "World"]
-print(random.choice(t_list))
+
+time = datetime.datetime.now()
+
+print(f"{time.strftime('%B')} {time.day}, {time.year}")
