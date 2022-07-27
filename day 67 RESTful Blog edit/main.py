90e383885b05a83417234824437287d758256c9a
Stefan Baltschun <StefanBaltschun@web.de>
2022-07-27 20:54:24 +0200

    start day 68 login and authentication

--- day 67 RESTful Blog edit/main.py
+++ day 67 RESTful Blog edit/main.py
@@ -92,10 +92,33 @@ def make_post():
         db.session.commit()
         return redirect(url_for("get_all_posts"))
 
+@app.route("/edit-post/<int:post_id>", methods=["POST", "GET"])
+def edit_post(post_id):
+    post = db.session.query(BlogPost).filter_by(id=post_id).first()
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", index=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
-
-
+@app.route("/delete/<post_id>", methods=["POST", "GET"])
+def delete_post(post_id):
+    post_to_delete = db.session.query(BlogPost).filter_by(id=post_id).first()
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts"))
 
 
 if __name__ == "__main__":
